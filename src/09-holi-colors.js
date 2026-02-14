e47cddb3c377223bfe80929bf7602e8e1754eff8
dipeshxhy <babasarkar260@gmail.com>
2026-02-14 07:27:40 +0000

    done with pure functions in holi-colors

--- src/09-holi-colors.js
+++ src/09-holi-colors.js
@@ -55,20 +55,72 @@
  */
 export function mixColors(color1, color2) {
   // Your code here
+  if(typeof color1 !=="object" || typeof color2 !=="object") return null
+  if(!color1 || !color2 ) return null
+  const newName = `${color1.name}-${color2.name}`
+  const r = Math.ceil((color1.r+color2.r)/2)
+  const b = Math.ceil((color1.b+color2.b)/2)
+  const g = Math.ceil((color1.g+color2.g)/2)
+  return {
+    name:newName,r,g,b
+  }
 }
 
 export function adjustBrightness(color, factor) {
   // Your code here
+  if(typeof color !=="object" ) return null
+  if(typeof factor !=="number" ) return null
+  if(!color ) return null
+  return {
+    ...color,
+    r:Math.round(color.r * factor) >255?255 :Math.round(color.r * factor),
+    g:Math.round(color.g * factor) >255?255 :Math.round(color.g * factor),
+    b:Math.round(color.b * factor) >255?255 :Math.round(color.b * factor)
+  }
+
 }
 
 export function addToPalette(palette, color) {
   // Your code here
+  if(!color ) return [...palette]
+  if(!palette || !Array.isArray(palette)|| palette.length ===0) return [color]
+  return [...palette, color]
+  
 }
 
 export function removeFromPalette(palette, colorName) {
   // Your code here
+  if(!Array.isArray(palette)) return []
+  console.log(palette)
+  return palette.filter(p=>p.name!==colorName)
 }
 
 export function mergePalettes(palette1, palette2) {
   // Your code here
+  // if(!palette1 || !palette2) return []
+  if(palette2 ===null) palette2=[]
+  if(palette1 ===null) palette1=[]
+  if(!palette1 && !palette2) return []
+  let unique=[]
+ 
+  if(palette2 && palette1){
+  const palettes =[...palette1, ...palette2]
+ const uniqueNames=[... new Set(palettes.map(pl=>pl.name))]
+
+  for(let i=0;i<uniqueNames.length;i++){
+unique.push(palettes.find(pl=>pl.name===uniqueNames[i]))
+
+  }
 }
+  return unique
+
+}
+
+ const red = () => ({ name: 'red', r: 255, g: 0, b: 0 });
+  const blue = () => ({ name: 'blue', r: 0, g: 0, b: 255 });
+  const green = () => ({ name: 'green', r: 0, g: 255, b: 0 });
+  const gray = () => ({ name: 'gray', r: 100, g: 100, b: 100 });  
+  const p1 = [red(), green()];
+        const result = mergePalettes(null, null);
+     
+      console.log(result)
